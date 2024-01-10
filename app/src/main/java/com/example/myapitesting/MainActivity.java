8357a09c550c0df78be2a0d1389d548a2dbe5d77
kumaranandtkr <115893905+kumaranandtkr@users.noreply.github.com>
2024-01-10 22:57:04 +0530

    First Commit

---
+++ app/src/main/java/com/example/myapitesting/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.myapitesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView tv = (TextView) findViewById(R.id.hello);
+
+        String url ="https://jsonplaceholder.typicode.com/todos/1";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    int userId = response.getInt("userId");
+                    int id = response.getInt("id");
+                    String title = response.getString("title");
+                    boolean completed = response.getBoolean("completed");
+
+
+                    tv.setText(userId+"\n"+id+"\n"+title+"\n"+completed);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tv.setText("error");
+            }
+        });
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+         requestQueue.add(jsonObjectRequest);
+    }
+}
